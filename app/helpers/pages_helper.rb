b61591e82b529c3f9e0244a8f703f261b4155b8f
Jason Zimdars <107673+jzimdars@users.noreply.github.com>
2024-03-20 15:34:24 -0500

    First stab at showing and editing text pages

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,8 @@ module PagesHelper
   def render_markdown(source)
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(source).html_safe
   end
+
+  def word_count(content)
+    pluralize content.split.size, "word"
+  end
 end
