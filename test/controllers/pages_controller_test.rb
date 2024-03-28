125dad18dbc5d5274abd49445ed7fb59ac18a395
Kevin McConnell <kevin@37signals.com>
2024-03-28 15:36:28 +0000

    Reload leaf before checking redirection in test
    The leaf will be pointing to a new leafable after the edit, so the
    test's instance will be out of date.
    
    CC @jzimdars

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -19,10 +19,9 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :ok
 
     put leafable_path(leaves(:welcome_page)), params: { page: { title: "Better welcome", body: "With even more interesting words." } }
-    # assert_redirected_to leafable_path(leaves(:welcome_page))
-    # Needs to account for updaged version of leafable
+    assert_redirected_to leafable_path(leaves(:welcome_page).reload)
 
-    updated_page = leaves(:welcome_page).reload.page
+    updated_page = leaves(:welcome_page).page
     assert_equal "Better welcome", updated_page.title
     assert_equal "With even more interesting words.", updated_page.body
   end
